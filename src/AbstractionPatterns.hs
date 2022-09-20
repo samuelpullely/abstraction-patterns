cbcb126b905f38734a22620042e26967074fe622
Samuel Pullely <samuelpullely@hotmail.com>
2022-09-20 17:14:46 +0200

    Define threeHops function

--- src/AbstractionPatterns.hs
+++ src/AbstractionPatterns.hs
@@ -1,1 +1,30 @@
 module AbstractionPatterns where
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+newtype Address = MkAddress Int
+    deriving (Eq, Ord, Show)
+
+-- type Map key val = [(key, val)]
+
+addressMapping :: Map Address Address
+addressMapping = M.fromList 
+    [(MkAddress 3, MkAddress 7),
+    (MkAddress 4, MkAddress 20),
+    (MkAddress 5, MkAddress 3),
+    (MkAddress 7, MkAddress 14),
+    (MkAddress 9, MkAddress 5),
+    (MkAddress 16, MkAddress 9)]
+
+threeHops :: Address -> Maybe String
+threeHops address0 = 
+    case M.lookup address0 addressMapping of
+        Nothing -> Nothing
+        Just address1 -> 
+            case M.lookup address1 addressMapping of
+                Nothing -> Nothing
+                Just address2 -> 
+                    case M.lookup address2 addressMapping of
+                        Nothing -> Nothing
+                        Just address3 -> Just (show address3)
